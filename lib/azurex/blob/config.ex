c814997309241f39db6f4ead0a82d71666ec9b49
kaaboaye <kaaboaye@gmail.com>
2019-11-30 17:51:45 +0100

    reformat

--- lib/azurex/blob/config.ex
+++ lib/azurex/blob/config.ex
@@ -10,10 +10,8 @@ defmodule Azurex.Blob.Config do
   def default_container, do: Keyword.get(conf(), :default_container)
   def storage_account_name, do: Keyword.get(conf(), :storage_account_name)
 
-  def storage_account_key do
-    key = Keyword.get(conf(), :storage_account_key)
-    if key, do: Base.decode64!(key)
-  end
+  def storage_account_key,
+    do: if(key = Keyword.get(conf(), :storage_account_key), do: Base.decode64!(key))
 
   def storage_account_connection_string,
     do: Keyword.get(conf(), :storage_account_connection_string)
