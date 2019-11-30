104098f9c850fc7af0265cda636160c743d69986
kaaboaye <kaaboaye@gmail.com>
2019-11-30 17:38:03 +0100

    remove hello world function

--- lib/azurex.ex
+++ lib/azurex.ex
@@ -2,17 +2,4 @@ defmodule Azurex do
   @moduledoc """
   Documentation for Azurex.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Azurex.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
