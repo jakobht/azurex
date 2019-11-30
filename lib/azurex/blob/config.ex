d442018c54b182d8965b573a1c558240d594fa18
kaaboaye <kaaboaye@gmail.com>
2019-11-30 17:53:02 +0100

    add types

--- lib/azurex/blob/config.ex
+++ lib/azurex/blob/config.ex
@@ -5,14 +5,20 @@ defmodule Azurex.Blob.Config do
 
   defp conf, do: Application.get_env(:azurex, __MODULE__, [])
 
+  @spec api_url :: String.t()
   def api_url, do: Keyword.get(conf(), :api_url)
 
+  @spec default_container :: String.t() | nil
   def default_container, do: Keyword.get(conf(), :default_container)
+
+  @spec storage_account_name :: String.t()
   def storage_account_name, do: Keyword.get(conf(), :storage_account_name)
 
+  @spec storage_account_key :: nil | binary
   def storage_account_key,
     do: if(key = Keyword.get(conf(), :storage_account_key), do: Base.decode64!(key))
 
+  @spec storage_account_connection_string :: String.t() | nil
   def storage_account_connection_string,
     do: Keyword.get(conf(), :storage_account_connection_string)
 end
