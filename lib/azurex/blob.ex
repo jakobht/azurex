f92c407ebcac9aa6f256db5477c62f4495654f5f
Jakob Haahr Taankvist <jakob_taankvist@hotmail.com>
2020-11-10 13:31:38 +0100

    List containers now works - just returns the XML

--- lib/azurex/blob.ex
+++ lib/azurex/blob.ex
@@ -4,13 +4,18 @@ defmodule Azurex.Blob do
 
   def list_containers do
     %HTTPoison.Request{
-      url: Config.api_url() <> "?comp=list"
+      url: Config.api_url() <> "/?comp=list"
     }
     |> SharedKey.sign(
       storage_account_name: Config.storage_account_name(),
       storage_account_key: Config.storage_account_key()
     )
     |> HTTPoison.request()
+    |> case do
+      {:ok, %{body: xml, status_code: 200}} -> {:ok, xml}
+      {:ok, err} -> {:error, err}
+      {:error, err} -> {:error, err}
+    end
   end
 
   @spec put_blob(String.t(), binary, String.t(), keyword) ::
@@ -29,7 +34,7 @@ defmodule Azurex.Blob do
       headers: [
         {"x-ms-blob-type", "BlockBlob"}
       ],
-      # Blob storage only answers when the whole file has been uploaded, so recv_timeout 
+      # Blob storage only answers when the whole file has been uploaded, so recv_timeout
       # is not applicable for the put request, so we set it to infinity
       options: [recv_timeout: :infinity]
     }
