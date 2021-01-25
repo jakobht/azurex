b8bdf562907547cf6f9f469b194801813e0f0fa1
Jakob Haahr Taankvist <jakob_taankvist@hotmail.com>
2021-01-25 12:59:00 +0100

    Version bumb

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Azurex.MixProject do
   def project do
     [
       app: :azurex,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       description: description(),
