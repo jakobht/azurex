0e35ff903b4d57929c40c2237713a7abaed75514
Shahne Rodgers <shahnerodgers@gmail.com>
2022-10-19 14:58:34 +1300

    Adds a method for creating containers
    This will be useful for setting up the CI pipeline:
    ```
    mix run -e "Azurex.Blob.Container.create(~s{test})"
    mix run -e "Azurex.Blob.Container.create(~s{integrationtestingcontainer})"
    ```

--- test/integration/container_integration_test.exs
+++ test/integration/container_integration_test.exs
@@ -26,4 +26,22 @@ defmodule Azurex.ContainerIntegrationTests do
                Container.head_container("Thi$isinvalid")
     end
   end
+
+  describe "create container" do
+    test "creates a new container" do
+      container_name = for _ <- 1..32, into: "", do: <<Enum.random('abcdefghijklmnopqrstuvwxyz')>>
+
+      assert {:error, :not_found} = Container.head_container(container_name)
+      assert {:ok, ^container_name} = Container.create(container_name)
+      assert {:ok, _} = Container.head_container(container_name)
+    end
+
+    test "returns an error if the container already exists" do
+      assert {:error, :already_exists} = Container.create(@integration_testing_container)
+    end
+
+    test "returns an error if the container name is invalid" do
+      assert {:error, %HTTPoison.Response{status_code: 400}} = Container.create("@$1")
+    end
+  end
 end
