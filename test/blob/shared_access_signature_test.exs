c8e73ae5272d0007ea7821c05eb23e52a2f13e6f
Christian Blavier <cblavier@gmail.com>
2022-10-20 15:56:10 +0200

    implemented SAS urls for blob storage

---
+++ test/blob/shared_access_signature_test.exs
@@ -0,0 +1,34 @@
+defmodule Azurex.Blob.SharedAccessSignatureTest do
+  use ExUnit.Case
+  import Azurex.Blob.SharedAccessSignature
+
+  setup_all do
+    Application.put_env(:azurex, Azurex.Blob.Config,
+      storage_account_name: "storage_account",
+      storage_account_key: Base.encode64("secretkey")
+    )
+  end
+
+  test "sas container url" do
+    assert sas_url(container(), "/", from: now()) ==
+             "https://storage_account.blob.core.windows.net/my_container?sv=2020-12-06&st=2022-10-10T10%3A10%3A00Z&se=2022-10-10T11%3A10%3A00Z&sr=c&sp=r&sig=NRjiSKbIhZPcu99pYt2bS015eQOMTX8WVIh3hJdj%2Fwk%3D"
+  end
+
+  test "sas blob url" do
+    assert sas_url(container(), blob(),
+             from: now(),
+             expiry: {:day, 2},
+             permissions: [:read, :write]
+           ) ==
+             "https://storage_account.blob.core.windows.net/my_container/folder/blob.mp4?sv=2020-12-06&st=2022-10-10T10%3A10%3A00Z&se=2022-10-12T10%3A10%3A00Z&sr=c&sp=rw&sig=Y2vH1nKzPkQhMnEXzz1m9Bz3o%2FPhyS1nOQp91B5GK9k%3D"
+  end
+
+  test "permissions order does not matter" do
+    assert sas_url(container(), blob(), from: now(), permissions: [:read, :add, :write, :delete]) ==
+             sas_url(container(), blob(), from: now(), permissions: [:delete, :write, :add, :read])
+  end
+
+  defp container, do: "my_container"
+  defp blob, do: "/folder/blob.mp4"
+  defp now, do: ~U[2022-10-10 10:10:00Z]
+end
