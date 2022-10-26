5b679862dc5095d928b9d6cdd9e53ee6a43221a6
Jakob Haahr Taankvist <jakob_taankvist@hotmail.com>
2022-10-26 11:35:26 +0200

    Work on making the integration tests run correctly

--- test/integration/container_integration_test.exs
+++ test/integration/container_integration_test.exs
@@ -5,6 +5,13 @@ defmodule Azurex.ContainerIntegrationTests do
   @moduletag integration: true
   @integration_testing_container "integrationtestingcontainer"
 
+  setup do
+    Application.put_env(:azurex, Azurex.Blob.Config,
+      storage_account_connection_string:
+        "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1"
+    )
+  end
+
   describe "head container" do
     test "returns ok when the container exists" do
       assert {:ok, _} = Container.head_container(@integration_testing_container)
